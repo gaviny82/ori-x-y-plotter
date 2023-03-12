1cb7457b15e528d239082e33f6d20f4b9b8ab860
Jincheng You <jincheng.you@outlook.com>
2023-03-12 21:06:20 +0000

    Add non-extrusion move support

--- src/helpers/gmatrix_extensions.py
+++ src/helpers/gmatrix_extensions.py
@@ -1,35 +1,40 @@
 from mecode import GMatrix
 
 
-def pen_down(g: GMatrix) -> None:
+def pen_down(g: GMatrix, show_z_paths: bool = False) -> None:
     """Lower the pen.
 
     Args:
         g (GMatrix): GMatrix instance to use for GCode generation.
+        show_z_paths (bool, optional): Whether to show the Z axis path. Defaults to False.
     """
 
     # This represents lowering the pen, but does not have any real effect as this plotter only prints in 2D.
-    if g.is_relative:
-        g.move(z=-3.3)
-    else:
-        g.move(x=g.current_position['x'], y=g.current_position['y'], z=0)
+    if show_z_paths:
+        if g.is_relative:
+            g.move(z=-3.3, rapid=True)
+        else:
+            g.move(x=g.current_position['x'], y=g.current_position['y'], z=0, rapid=True)
 
     # This command actually lowers the pen.
     g.write("M9")
 
 
-def pen_up(g: GMatrix) -> None:
+def pen_up(g: GMatrix, show_z_paths: bool = False) -> None:
     """Raise the pen.
 
     Args:
         g (GMatrix): GMatrix instance to use for GCode generation.
+        show_z_paths (bool, optional): Whether to show the Z axis path. Defaults to False.
     """
 
     # This represents raising the pen, but does not have any real effect as this plotter only prints in 2D.
-    if g.is_relative:
-        g.move(z=3.3)
-    else:
-        g.move(x=g.current_position['x'], y=g.current_position['y'], z=3.3)
+    if show_z_paths:
+        if g.is_relative:
+            g.move(z=3.3, rapid=True)
+        else:
+            g.move(x=g.current_position['x'],
+                y=g.current_position['y'], z=3.3, rapid=True)
 
     # This command actually raises the pen.
     g.write("M8")
